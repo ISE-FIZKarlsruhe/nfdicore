be3d271e9ce702660a78c8e9f7e493b36e229236
joergwa <waitelonis@yovisto.com>
2025-02-05 10:53:11 +0100

    release prep

--- src/ontology/release.sh
+++ src/ontology/release.sh
@@ -1,13 +1,12 @@
 
 
-VERSION=3.0.0-alpha
+VERSION=3.0.0-alpha1
 PRIOR_VERSION=2.1.0
 ONTBASE=https://nfdi.fiz-karlsruhe.de/ontology 
 ANNOTATE_ONTOLOGY_VERSION="annotate -V $ONTBASE/$VERSION/\$@ --annotation owl:versionInfo $VERSION"
 
 
 sh run.sh make clean
-sh run.sh make 
 
 sh run.sh make VERSION=$VERSION ONTBASE=$ONTBASE ANNOTATE_ONTOLOGY_VERSION="$ANNOTATE_ONTOLOGY_VERSION" prepare_release
 #sh run.sh make VERSION=$VERSION ONTBASE=$ONTBASE prepare_release
