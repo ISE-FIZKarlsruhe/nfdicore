a8f7c2853cbcb37d337a951e69b122ae4f47de3c
joergwa <waitelonis@yovisto.com>
2025-10-24 13:59:03 +0200

    release preps

--- src/ontology/release.sh
+++ src/ontology/release.sh
@@ -1,7 +1,7 @@
 
 
-VERSION=3.0.2
-PRIOR_VERSION=3.0.1
+VERSION=3.0.3
+PRIOR_VERSION=3.0.2
 ONTBASE=https://nfdi.fiz-karlsruhe.de/ontology 
 ANNOTATE_ONTOLOGY_VERSION="annotate -V $ONTBASE/$VERSION/\$@ --annotation owl:versionInfo $VERSION"
 
@@ -12,9 +12,10 @@ sh run.sh make VERSION=$VERSION ONTBASE=$ONTBASE ANNOTATE_ONTOLOGY_VERSION="$ANN
 #sh run.sh make VERSION=$VERSION ONTBASE=$ONTBASE prepare_release
 
 sh run.sh make VERSION=$VERSION ONTBASE=$ONTBASE ANNOTATE_ONTOLOGY_VERSION="$ANNOTATE_ONTOLOGY_VERSION" nfdicore-extension.ttl
+
 sh run.sh make VERSION=$VERSION PRIOR_VERSION=$PRIOR_VERSION update-ontology-annotations
 
 cp nfdicore-extension.ttl ../../profiles/nfdicore-extension.ttl
 
 # finally refresh imports again, so that version IRIs are updated back to "normal". 
-sh run.sh make IMP=false MIR=false refresh-imports
+#sh run.sh make IMP=false MIR=false refresh-imports
